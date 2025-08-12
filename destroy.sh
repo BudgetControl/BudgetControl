8e421becf73e1a2d26c84c762237dbbbdaff855b
Marco De Felice <marco.defelice@budgetcontrol.cloud>
2025-08-12 18:49:45 +0200

    update docker compose

--- destroy.sh
+++ destroy.sh
@@ -8,46 +8,41 @@ git submodule update --init --recursive
 
 echo "Stop container"
 
-docker container stopb udgetcontrol-core
-docker container stopb udgetcontrol-core php artisan migrate
-docker container stopb udgetcontrol-core php artisan optimize
-
-docker container stopb udgetcontrol-gateway
-docker container stopb udgetcontrol-gateway php artisan optimize
-
-docker container stopb udgetcontrol-ms-stats
-docker container stopb udgetcontrol-ms-authentication
-docker container stopb udgetcontrol-ms-jobs
-docker container stopb udgetcontrol-ms-workspace
-docker container stopb udgetcontrol-ms-budget
-docker container stopb udgetcontrol-ms-entries
-docker container stopb udgetcontrol-ms-wallets
-docker container stopb udgetcontrol-ms-searchengine
-docker container stopb udgetcontrol-ms-labels
-docker container stopb udgetcontrol-ms-debt
-docker container stopb udgetcontrol-ms-savings
-docker container stopb udgetcontrol-ms-goals
-docker container stopb udgetcontrol-ms-notifications
+docker container stop budgetcontrol-core
+docker container stop budgetcontrol-gateway
+docker container stop budgetcontrol-ms-stats
+docker container stop budgetcontrol-ms-authentication
+docker container stop budgetcontrol-ms-jobs
+docker container stop budgetcontrol-ms-workspace
+docker container stop budgetcontrol-ms-budget
+docker container stop budgetcontrol-ms-entries
+docker container stop budgetcontrol-ms-wallets
+docker container stop budgetcontrol-ms-searchengine
+docker container stop budgetcontrol-ms-labels
+docker container stop budgetcontrol-ms-debt
+docker container stop budgetcontrol-ms-savings
+docker container stop budgetcontrol-ms-goals
+docker container stop budgetcontrol-ms-notifications
 
 echo "Remove container"
 
-docker container rm udgetcontrol-core
-docker container rm udgetcontrol-core php artisan migrate
-docker container rm udgetcontrol-core php artisan optimize
+docker container rm budgetcontrol-core
+docker container rm budgetcontrol-core 
+docker container rm budgetcontrol-core php artisan optimize
 
-docker container rm udgetcontrol-gateway
-docker container rm udgetcontrol-gateway php artisan optimize
+docker container rm budgetcontrol-gateway
+docker container rm budgetcontrol-gateway php artisan optimize
 
-docker container rm udgetcontrol-ms-stats
-docker container rm udgetcontrol-ms-authentication
-docker container rm udgetcontrol-ms-jobs
-docker container rm udgetcontrol-ms-workspace
-docker container rm udgetcontrol-ms-budget
-docker container rm udgetcontrol-ms-entries
-docker container rm udgetcontrol-ms-wallets
-docker container rm udgetcontrol-ms-searchengine
-docker container rm udgetcontrol-ms-labels
-docker container rm udgetcontrol-ms-debt
-docker container rm udgetcontrol-ms-savings
-docker container rm udgetcontrol-ms-goals
-docker container rm udgetcontrol-ms-notifications
+docker container rm budgetcontrol-ms-stats
+docker container rm budgetcontrol-ms-authentication
+docker container rm budgetcontrol-ms-jobs
+docker container rm budgetcontrol-ms-workspace
+docker container rm budgetcontrol-ms-budget
+docker container rm budgetcontrol-ms-entries
+docker container rm budgetcontrol-ms-wallets
+docker container rm budgetcontrol-ms-searchengine
+docker container rm budgetcontrol-ms-labels
+docker container rm budgetcontrol-ms-debt
+docker container rm budgetcontrol-ms-savings
+docker container rm budgetcontrol-ms-goals
+docker container rm budgetcontrol-ms-notifications
